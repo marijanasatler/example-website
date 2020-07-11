103af930fd214436499bf5cd9cf5539288470ed1
Marijana Satler <marijanasatler@gmail.com>
2020-07-11 13:29:47 +0200

    Updates

---
+++ precache-manifest.70c45dd267f787ef291a867d55286a8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4ecea306742223f68a9356fd9aace3",
+    "url": "/example-website/index.html"
+  },
+  {
+    "revision": "7e75bf85c8a7ea64c586",
+    "url": "/example-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "833c4ce5c7534d54bbbc",
+    "url": "/example-website/static/js/2.718153cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example-website/static/js/2.718153cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e75bf85c8a7ea64c586",
+    "url": "/example-website/static/js/main.8287c4c6.chunk.js"
+  },
+  {
+    "revision": "e8557c3e971e8a700f46",
+    "url": "/example-website/static/js/runtime-main.9720a5af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
